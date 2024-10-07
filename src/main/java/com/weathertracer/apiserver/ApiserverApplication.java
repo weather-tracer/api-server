c62ca5e079c4cb400d089f878fe7228db5de48d8
Jo YoHan <whdygks210@gmail.com>
2024-10-07 23:02:54 +0900

    init project

---
+++ src/main/java/com/weathertracer/apiserver/ApiserverApplication.java
@@ -0,0 +1,13 @@
+package com.weathertracer.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiserverApplication.class, args);
+	}
+
+}
